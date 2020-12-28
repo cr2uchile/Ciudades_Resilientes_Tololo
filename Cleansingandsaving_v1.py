bfecb8c5a7804576d9b3b2ba26d53ae8be5c8070
cfmenares <73909165+cfmenares@users.noreply.github.com>
2020-12-28 12:26:49 -0300

    Primera versión para lectura datos DMC
    Codigo desarrollado por L.Gallardo 
    
    Readingandsaving: Le datos usando Pandas y Numpy para el manejo de datos.
    Cleansingandsaving: Limpia datos usando criterios instrumentales y un máximo definido en ozono
    Info_Plotting            : Liberia para la confección de figuras usando  import matplotlib.pyplot as plt

---
+++ Cleansingandsaving_v1.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 13:01:43 2020
+
+@author: laura Gallardo & Camilo Menares
+
+Reads, cleans and samples TOLOLO data 
+Data --ozone only--for the period 2013-2020 were downloaded from http://ebas.nilu.no/
+This set corresponds to hourly averaged values
+
+Data (ozone, water vapor, radiation, wind, etc)for the period 1995-2013 
+were obtained from the Chilean Meteorological Office
+thanks to Dr. Carmen Vega (carmen.vega@dgac.gob.cl) and colleagues
+"""
+
+
+
+#Importing libraries
+
+import pandas as pd              # Data structures
+import matplotlib.pyplot as plt  # Plotting 
+import os as os                  # Directory management    
+import numpy as np               # Numerics   
+import datetime
+import info_Plotting  #Import key settings for the following graphs and data
+# Libreria creada para definir las rutas y los string de los Modelos
+from info_Plotting import FHIST
+from info_Plotting import FSERIES
+
+
+
+
+#Exploring EBAS data
+# The following file was read using Readingandsaving.py
+# It consists of a dataframe containg hourly values of ozone and the corresponding 
+# standard deviation
+
+# The ozone sensor is a refur-bished ozone photometer (Thermo Scientific, TE49c) since 2013
+# see Anet et al, 2017 doi:10.5194/acp-17-6477-2017 for details, including
+# https://acp.copernicus.org/articles/17/6477/2017/acp-17-6477-2017-supplement.pdf
+# See https://assets.thermofisher.com/TFS-Assets/EPM/manuals/EPM-manual-Model%2049i.pdf
+# 
+#Thus 
+# Response time: 20s with 10s lag
+# Precision: 1ppbv
+# Selectable Full Scale Range: 0.05--200 ppm or 0.1--400 mg/m^3
+# Lower limit of detection: 1.0ppb (zero noise 0.5 ppb RMS)
+
+
+# orig = os.getcwd() #Says where the file is
+# fn=orig+'\\DATA\\'+'EBAS-O3H-2013-2019'
+# df = pd.read_csv(fn) 
+# df.rename(columns = {'Unnamed: 0':'Date'}, inplace = True)
+
+# df_orig_ebas=df #Original EBAS data set
+
+# # Plotting time series
+# FSERIES('O3','EBAS',df,1) #PENDING TIME AXES
+
+# #Creating histograms of data
+# #FHIST(spec,dbname,df,Nbins, ext=None)
+# FHIST('O3','EBAS',df,50) #PENDING Anchored text
+
+# #These data ave been subject to evaluation we do not "clean"
+
+# dfH_ebas=df
+
+############################
+#Exploring DMC data
+# The following file was read using Readingandsaving.py
+# It consists of a dataframe containg hourly values of ozone and the corresponding 
+# standard deviation
+orig = os.getcwd() #Says where the file is
+fn=orig+'/Data/'+'DMC-O3_RH_15m_dmc-1995-2012'  # cambiar fn linea inferior
+#fn=orig+'\\DATA\\'+'DMC-O3_RH_15m_dmc-1995-2012'  
+
+df = pd.read_csv(fn,index_col=0,parse_dates=True) 
+#df.rename(columns = {'Unnamed: 0':'Date'}, inplace = True)
+df_orig_dmc=df  #Original time series, before cleansing
+
+
+#Plotting time series
+
+FSERIES('O3','DMC',df,1) #PENDING TIME AXES
+# The ozone sensor was a TECO  49-003  analyzer, between 1995-2013
+# see Anet et al, 2017 doi:10.5194/acp-17-6477-2017 for details
+# https://www.eol.ucar.edu/instruments/thermo-environmental-instruments-model-49-ozone-analyzer
+#Thus 
+# Response time: 20s with 10s lag
+# Precision: 1ppbv
+# Selectable Full Scale Range: 0.05--200 ppm or 0.1--400 mg/m^3
+# Lower limit of detection: 1.0ppb (zero noise 0.5 ppb RMS)
+# Other measurement characteristics (comments on signal/noise, bias limits, etc)
+
+#Removing values below detection limit or the assumed range of calibration, i.e. 5 ppbv
+
+df.O3_ppbv[df.O3_ppbv <5] = np.nan
+
+# # WARNING
+# See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+#   df.O3_ppbv[df.O3_ppbv <5] = np.nan
+# C:\Users\laura\Desktop\DataVis\TOLOLO\Cleansingandsaving.py:100: SettingWithCopyWarning: 
+# A value is trying to be set on a copy of a slice from a DataFrame
+
+#The time series evidences calibration spikes. In lack of the record of dates of calibration, 
+# we remove values above 65 ppbv, considering the data distribution of EBAS hourly values
+
+df.O3_ppbv[df.O3_ppbv > 200] = np.nan
+
+FSERIES('O3','DMC',df,1) #PENDING TIME AXES
+
+
+orig = os.getcwd() 
+
+#fn=orig+'\\DATA\\'+'DMC-O3_RH_15m_dmc-1995-2012_clear'
+
+fn=orig+'/Data/DMC-O3_RH_15m_dmc-1995-2012_clear'
+df.to_csv(fn)
+
+
+#Creating histograms of data
+#FHIST(spec,dbname,df,Nbins, ext=None)
+#FHIST('O3','DMC',df,50) #PENDING Anchored text
+
+
+
+
+#Resamplig to hourly values and calculating the corresponding standard deviation
+
+# dfH=df.resample('H').mean()
+# dfstd=df.resample('H').std()
+#ERROR: COMPLAINS
+# C:\Users\laura\Desktop\DataVis\TOLOLO\Cleansingandsaving.py:100: SettingWithCopyWarning: 
+# A value is trying to be set on a copy of a slice from a DataFrame
+
+# See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+#   df.O3_ppbv[df.O3_ppbv > 68] = np.nan
+# C:\Users\laura\anaconda3\lib\site-packages\numpy\lib\histograms.py:839: RuntimeWarning: invalid value encountered in greater_equal
+#   keep = (tmp_a >= first_edge)
+# C:\Users\laura\anaconda3\lib\site-packages\numpy\lib\histograms.py:840: RuntimeWarning: invalid value encountered in less_equal
+#   keep &= (tmp_a <= last_edge)
+# Traceback (most recent call last):
+
+#   File "C:\Users\laura\Desktop\DataVis\TOLOLO\Cleansingandsaving.py", line 112, in <module>
+#     dfH=df.resample('H').mean()
+
+#   File "C:\Users\laura\anaconda3\lib\site-packages\pandas\core\generic.py", line 8115, in resample
+#     level=level,
+
+#   File "C:\Users\laura\anaconda3\lib\site-packages\pandas\core\resample.py", line 1270, in resample
+#     return tg._get_resampler(obj, kind=kind)
+
+#   File "C:\Users\laura\anaconda3\lib\site-packages\pandas\core\resample.py", line 1404, in _get_resampler
+#     "Only valid with DatetimeIndex, "
+
+# TypeError: Only valid with DatetimeIndex, TimedeltaIndex or PeriodIndex, but got an instance of 'RangeIndex'
+
+#Creating data series of hourly ozone and corresponding standard deviation
---
+++ Readingandsaving_v1.py
@@ -0,0 +1,310 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 18:29:03 2020
+
+@author: laura Gallardo, inspired by Camilo Menares
+
+Reading and saving Tololo ozone data
+
+Data for the period 2013-2020 was downloaded from http://ebas.nilu.no/
+This set corresponds to hourly averaged values
+
+Data for the period 1995-2013 was obtained from the Chilean Meteorological Office
+thanks to Dr. Carmen Vega (carmen.vega@dgac.gob.cl) and colleagues
+
+"""
+
+
+#Importing libraries
+
+import pandas as pd              # Data structures
+import matplotlib.pyplot as plt  # Plotting 
+import os as os                  # Directory management    
+import numpy as np               # Numerics   
+import datetime
+
+#Reading data downloaded from  http://ebas.nilu.no/
+#Hourly averages
+
+
+def leer_ebas(inicio,fin):
+    """    
+    Parameters
+    ----------
+    inicio : string
+        Fecha de inicio del documento ej '20130101'
+    fin : TYPE
+        Fecha final del documento ej '20140101'
+    spec: string
+          Reads either relative humidity or ozone in ppbv
+    Returns
+    -------
+    df : Dataframe
+        Archivo pandas Df con el año de lectura conteniendo O3 en mug/m3 y pbbv mas std obtenudas desde
+        http://ebas.nilu.no/Pages/DataSetList.aspx?key=A4C1704036474C47AED6FA75E2A99882.
+        El formato de los datos está descrito en NASA Ames 1001 format.: https://cloud1.arc.nasa.gov/solve/archiv/archive.tutorial.html
+            
+
+    """
+    orig = os.getcwd() #Says where the file is
+    datadir=orig+'/Datos'    # Cambiar datadir
+    #datadir=orig+'\\DATA\\DB-EBAS'
+
+    # print(datadir)
+    # os.chdir(datadir)
+    # #os.chdir(HOME+'\Datos')       # Ingresa a la carpeta de datos, puede tener error en Windows hint: cambiar / por \
+
+    if inicio=='20190101':
+        name_data = 'CL0001R.'+inicio+'000000.20200630060000.uv_abs.ozone.air.1y.1h.CL01L_TEI49C_no72417371.CL01L_uv_abs.lev2.nas' # Define el nombre de los archivos a leer
+    else:
+        name_data = 'CL0001R.'+inicio+'000000.20201102112043.uv_abs.ozone.air.1y.1h.CL01L_TEI49C_no72417371.CL01L_uv_abs.lev2.nas'
+   
+    tiempo = pd.date_range(inicio,fin, freq= 'H')                                   # Vectir de tiempo con los input de inicio y fin
+    input_data = pd.read_csv(datadir+'\\'+ name_data,decimal=".", delimiter=r"\s+", header =76);  # lectura de datos
+    #Data structure
+    #[8760 rows x 7 columns]>
+    #starttime     endtime        O3    O3.1     O3.2   O3.3   flag
+    #
+    
+    input_data = input_data.rename(columns={input_data.keys()[2]: "O3_ug/m3", input_data.keys()[3]: "O3_ppbv",
+                               input_data.keys()[4]: "O3_ug/m3_std" , input_data.keys()[5]: "O3_ppbv_std", 
+                               input_data.keys()[6]: "flag"})                        # Redefine las columnas del dataframe
+    
+
+    # Remplazar no values por NaN
+    
+# input_data["O3_ug/m3"].replace(9999.999, np.nan, inplace=True)                   
+    input_data["O3_ppbv"].replace(999.99, np.nan, inplace=True)
+#    input_data["O3_ug/m3_std"].replace(999.9989999999999, np.nan, inplace=True)
+    input_data["O3_ppbv_std"].replace(99.99, np.nan, inplace=True)
+    
+
+    # Generar series indexadas con tiempo
+    
+#    O3_ug = pd.Series(input_data["O3_ug/m3"].values, index=tiempo[0:-1])             
+    O3_ppbv = pd.Series(input_data["O3_ppbv"].values, index=tiempo[0:-1])
+    
+    
+#    O3_ug_2 = pd.Series(input_data["O3_ug/m3_std"].values, index=tiempo[0:-1])
+    O3_ppbv_2 = pd.Series(input_data["O3_ppbv_std"].values, index=tiempo[0:-1])
+
+    # Forma estructura dataframe final
+    
+
+#    df = pd.DataFrame({ "O3_ug/m3" : O3_ug, "O3_ppbv" : O3_ppbv, "O3_ug/m3_std" : O3_ug_2, "O3_ppbv_std" : O3_ppbv_2 })
+    
+    df = pd.DataFrame({ "O3_ppbv" : O3_ppbv, "O3_ppbv_std" : O3_ppbv_2 })
+
+    
+    return df                   
+
+
+
+#Reading data provided by DMC 
+#15 min averages on yearly files
+
+
+def leer_dmc(inicio,fin,tipo,filtro=True):
+    """    
+    Parameters
+    ----------
+    inicio : string
+        Fecha de inicio del documento ej '20130101'
+    fin : TYPE
+        Fecha final del documento ej '20140101'
+    tipo: indicates type of data structure either 118 or 119
+    
+    118 is organized as 24 columns indicating:
+    1) Array Identifier; 
+    2) Julian Day; 
+    3) Standard Time; 
+    4) Station ID; 
+    5) Battery Voltage; 
+    6) Wind speed (m/s); 
+    7) Wind Direction (0-360°);
+    8) Average internal temperature (C); 
+    9) Average ambient temperature(C);
+    10) Average relative humidity(%); 
+    11) Average solar radiation licor (W/m2);
+    12) Average solar radiation diffuse Kipp-Zonen(W/m2);
+    13) Average solar radiation incident Kipp-Zonen(W/m2);
+    14) Average net radiation Shenk (W/m2);
+    15) Average UV-B radiation Yankee-Erythermal (mW/m2)
+    16) Average UV-B radiation Yankee Total UV-B (mW/m2)
+    17) Average pressure (mb); 
+    18) Average ozone (ppb)[possibly v]
+    19) Total precipitation (mm)); 
+    20) Standard deviation of wind speed (m/s))
+    21) Average wind speed (m/s); 
+    22) Average wind vector magnitude; 
+    23) Average wind vector direction; 
+    24) Standard deviation of wind direction
+    
+    119 is organized as 31 columns indicating:
+    1) Array Identifier; 
+    2) Year; 
+    3) Julian Day; 
+    4) Standard Time; 
+    5) Station ID;
+    6) Signature;
+    7) Battery Voltage; 
+    8) Instantaneous solar radiation licor (W/m2);
+    9) Instantaneous solar radiation diffuse Kipp-Zonen(W/m2);
+    10) Instantaneous solar radiation incident Kipp-Zonen(W/m2);
+    11) Instantaneous net radiation Shenk (W/m2);
+    12) Instantaneous UV-B radiation Yankee-Erythermal (mW/m2)
+    13) Instantaneous UV-B radiation Yankee Total UV-B (mW/m2)
+    14) Average internal temperature (C); 
+    15) Average ambient temperature(C);
+    16) Average relative humidity(%);
+    17) Average solar radiation diffuse Kipp-Zonen(W/m2);
+    18) Average solar radiation incident Kipp-Zonen(W/m2);
+    19) Average net radiation Shenk (W/m2);
+    20) Average UV-B radiation Yankee-Erythermal (mW/m2)
+    21) Average UV-B radiation Yankee Total UV-B (mW/m2)
+    22) Average pressure (mb); 18) Average ozone (ppb)[possibly v]
+    23) Average pressure (mb)    
+    24) Average ozone (ppb) 
+    25) Total precipitation (mm)); 
+    26) Standard deviation of wind speed (m/s))
+    27) Standard deviation of ozone (ppb)
+    28) Average wind speed (m/s))
+    29) Average wind vector magnitude; 
+    30) Average wind vector direction; 
+    31) Standard deviation of wind direction
+     
+    Returns
+    -------
+    df : Dataframe
+        Archivo pandas Df con el año de lectura conteniendo O3 en mug/m3 y pbbv mas std obtenudas desde
+        http://ebas.nilu.no/Pages/DataSetList.aspx?key=A4C1704036474C47AED6FA75E2A99882.
+
+    """
+    orig = os.getcwd() #Says where the file is
+    datadir=orig+'/Data'         # cambiar datadir
+    #datadir=orig+'\\DATA\\DB-EBAS'
+    
+    if inicio=='1997': 
+        if tipo==118:
+            name_data='ET'+inicio+'_1'+'.csv'
+        elif tipo==119:
+            name_data='ET'+inicio+'_2'+'.csv'
+    else:
+        name_data='ET'+inicio+'.csv'
+    
+#    print(name_data)
+    if tipo==118:
+        input_data = pd.read_csv(datadir+'/'+ name_data,decimal=",", delimiter=r";", header =0)  # lectura de datos
+    elif tipo==119:
+        input_data = pd.read_csv(datadir+'/'+ name_data, decimal=",", delimiter=r";",  header =0, na_values= '?') 
+        
+    #Renaming column names
+    if tipo==118:
+        input_data = input_data.rename(columns={input_data.keys()[9]: "RH(%)",input_data.keys()[17]: "O3_ppbv"})                            
+    elif tipo==119:
+        input_data = input_data.rename(columns={input_data.keys()[15]: "RH(%)",input_data.keys()[23]: "O3_ppbv"})
+    
+#    input_data["RH(%)"].replace(-99999, np.nan, inplace=True)    
+#   input_data["O3_ppbv"].replace(6999, np.nan, inplace=True)  
+    #Changing dates
+        
+    if tipo==118:
+        j=input_data['2']  # Julian Day
+        h_aux=input_data['3']
+    elif tipo==119:
+        j=input_data['3']  # Julian Day
+        h_aux=input_data['4']
+    
+    
+    h = list()
+    for i in h_aux:
+        #print(i)
+        if len(str(i)) < 3:
+            h.append('00'+str(i))
+        elif len(str(i)) < 4:
+            h.append('0'+str(i))
+        else:
+            h.append(str(i))
+            
+    h[h == '2400'] = '0000'
+           
+    vector_t=list()
+    
+    for i in range(h_aux.size):
+        if h[i]== '2400':
+            h[i]= '0000'
+        tiempo2=datetime.datetime.strptime(inicio +'-'+str(j[i])+'-'+h[i][0:2]+'-'+h[i][2:5], '%Y-%j-%H-%M')
+        vector_t.append(tiempo2)
+    
+    #From datetime to stamptime
+    
+    tiempo=pd.DatetimeIndex(vector_t)
+    
+    # Generar series indexadas con tiempo
+    
+    O3_ppbv = pd.Series(input_data["O3_ppbv"].values, index=tiempo)
+    RH_perc = pd.Series(input_data["RH(%)"].values, index=tiempo)
+
+        
+    # Forma estructura dataframe final
+        
+   
+    df = pd.DataFrame({ "O3_ppbv" : O3_ppbv, "RH_perc" : RH_perc })
+    
+    if filtro == True : 
+        df[df<df.quantile(0.01)] = np.nan
+        df[df>df.quantile(0.99)] = np.nan
+   
+        df[df < 0] = np.nan
+        df.RH_perc[df.RH_perc > 100] = np.nan
+    return df    
+
+##################################################
+
+
+
+#Reading old data 1995-2012
+dfold = pd.DataFrame()
+
+#From 1995 to 1996
+for i in range(1995,1997):
+    var=leer_dmc(str(i),str(i+1),118) 
+    dfold = pd.concat([dfold,var])
+    
+# 1997 tewo sections
+var=leer_dmc('1997','1998',118)
+dfold = pd.concat([dfold,var])
+
+var=leer_dmc('1997','1998',119)
+dfold = pd.concat([dfold,var])
+#1998 on
+for i in range(1998,2013):
+    var=leer_dmc(str(i),str(i+1),119) 
+    dfold = pd.concat([dfold,var])
+
+
+dfdmc_O3_RH_15m=dfold
+
+
+orig = os.getcwd() 
+fn= orig+'/Data'   ## cambiar :  fn = orig+ '\\DATA\\'
+#fn = orig+ '\\DATA\\'
+dfdmc_O3_RH_15m.to_csv(fn+'/DMC-O3_RH_15m_dmc-1995-2012')
+    
+
+#Resampling
+    
+# dfoldH1=dfold.resample('H').mean()
+# dfoldH2=dfold.resample('H').std()
+
+# dfold = pd.DataFrame()
+# dfold=pd.concat([dfold,dfoldH1.O3_ppbv],axis=0)
+# dfold=pd.concat([dfold,dfoldH2.O3_ppbv],axis=1)
+
+# #Saving data frames
+    
+# orig = os.getcwd() #Says where the file is
+# ruta=orig+'\\DATA\\'
+# df.to_csv(ruta+'O3-2013-2019')
+# #Otherwise: df.to_excel("output.xlsx") 
+
