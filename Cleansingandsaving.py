c6ace252ee7d82cfab59abc607a1d2d7d29bd176
Sebastián Villalón <70820491+Svillalon@users.noreply.github.com>
2021-01-07 18:28:29 -0300

    Se agregan funciones
    Se implementan funciones clean_series_demo y clean_series

--- Cleansingandsaving.py
+++ Cleansingandsaving.py
@@ -8,7 +8,7 @@ Reads, cleans and samples TOLOLO data
 Data --ozone only--for the period 2013-2020 were downloaded from http://ebas.nilu.no/
 This set corresponds to hourly averaged values
 
-Data (ozone, water vapor, radiation, wind, etc)for the period 1995-2013 
+Data (ozone, water vapor, radiation, wind, etc)for the period 1995-2013
 were obtained from the Chilean Meteorological Office
 thanks to Dr. Carmen Vega (carmen.vega@dgac.gob.cl) and colleagues
 """
@@ -18,13 +18,14 @@ thanks to Dr. Carmen Vega (carmen.vega@dgac.gob.cl) and colleagues
 #Importing libraries
 
 import pandas as pd              # Data structures
-import matplotlib.pyplot as plt  # Plotting 
-import os as os                  # Directory management    
-import numpy as np               # Numerics   
+import matplotlib.pyplot as plt  # Plotting
+import os as os                  # Directory management
+import numpy as np               # Numerics
 import datetime
 import info_Plotting  #Import key settings for the following graphs and data
 # Libreria creada para definir las rutas y los string de los Modelos
 from info_Plotting import FHIST
+from info_Plotting import FHIST2
 from info_Plotting import FSERIES
 
 
@@ -47,19 +48,20 @@ from info_Plotting import FSERIES
 # Lower limit of detection: 1.0ppb (zero noise 0.5 ppb RMS)
 
 
-# orig = os.getcwd() #Says where the file is
-# fn=orig+'\\DATA\\'+'EBAS-O3H-2013-2019'
-# df = pd.read_csv(fn) 
-# df.rename(columns = {'Unnamed: 0':'Date'}, inplace = True)
+orig = os.getcwd() #Says where the file is
+fn = orig+'\\DATA\\'+'EBAS-O3H-2013-2019.csv'
+df = pd.read_csv(fn, parse_dates=True, index_col=0)
+df.rename(columns={'Unnamed: 0': 'Date'}, inplace=True)
 
-# df_orig_ebas=df #Original EBAS data set
+df_orig_ebas = df #Original EBAS data set
 
 # # Plotting time series
-# FSERIES('O3','EBAS',df,1) #PENDING TIME AXES
+FSERIES('O3', 'EBAS', df, 1) #PENDING TIME AXES
 
 # #Creating histograms of data
 # #FHIST(spec,dbname,df,Nbins, ext=None)
-# FHIST('O3','EBAS',df,50) #PENDING Anchored text
+FHIST('O3', 'EBAS', df, 50) #PENDING Anchored text
+
 
 # #These data ave been subject to evaluation we do not "clean"
 
@@ -72,16 +74,18 @@ from info_Plotting import FSERIES
 # standard deviation
 orig = os.getcwd() #Says where the file is
 #fn=orig+'/Data/'+'DMC-O3_RH_15m_dmc-1995-2012'  # cambiar fn linea inferior
-fn=orig+'\\DATA\\'+'DMC-O3_RH_15m_dmc-1995-2012'  
+fn = orig+'\\DATA\\'+'DMC-O3_RH_15m_dmc-1995-2013.csv'  
 
 df = pd.read_csv(fn,index_col=0,parse_dates=True) 
 #df.rename(columns = {'Unnamed: 0':'Date'}, inplace = True)
-df_orig_dmc=df  #Original time series, before cleansing
+df_orig_dmc = df  #Original time series, before cleansing
 
 
 #Plotting time series
 
-FSERIES('O3','DMC',df,1) #PENDING TIME AXES
+FSERIES('O3', 'DMC', df, 1) #PENDING TIME AXES
+#FHIST('O3', 'DMC', df, 50)
+def clean_series_demo(Min, Max, df):
 # The ozone sensor was a TECO  49-003  analyzer, between 1995-2013
 # see Anet et al, 2017 doi:10.5194/acp-17-6477-2017 for details
 # https://www.eol.ucar.edu/instruments/thermo-environmental-instruments-model-49-ozone-analyzer
@@ -94,7 +98,7 @@ FSERIES('O3','DMC',df,1) #PENDING TIME AXES
 
 #Removing values below detection limit or the assumed range of calibration, i.e. 5 ppbv
 
-df.O3_ppbv[df.O3_ppbv <5] = np.nan
+    df.O3_ppbv[df.O3_ppbv < Min] = np.nan
 
 # # WARNING
 # See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
@@ -105,21 +109,36 @@ df.O3_ppbv[df.O3_ppbv <5] = np.nan
 #The time series evidences calibration spikes. In lack of the record of dates of calibration, 
 # we remove values above 65 ppbv, considering the data distribution of EBAS hourly values
 
-df.O3_ppbv[df.O3_ppbv > 200] = np.nan
+    df.O3_ppbv[df.O3_ppbv > Max] = np.nan
+    FSERIES('O3', 'DMC', df, 1)
+    FHIST2('O3', 'DMC', df, 50)
+    series = df.O3_ppbv
+    mean = series.mean()
+    std = series.std()
+    return mean, std
 
-FSERIES('O3','DMC',df,1) #PENDING TIME AXES
+
+def clean_series(Min, Max, df):
+    df.O3_ppbv[df.O3_ppbv < Min] = np.nan
+    df.O3_ppbv[df.O3_ppbv > Max] = np.nan
 
 
-orig = os.getcwd() 
+clean_series(5, 100, df)
+FSERIES('O3', 'DMC', df, 1)
+FHIST2('O3', 'DMC', df, 50)
+#PENDING TIME AXES
 
-fn=orig+'\\DATA\\'+'DMC-O3_RH_15m_dmc-1995-2012_clear'
+#df.O3_ppbv[df.O3_ppbv <5] = np.nan
+orig = os.getcwd()
+
+fn=orig+'\\DATA\\'+'DMC-O3_RH_15m_dmc-1995-2013_clear.csv'
 #fn=orig+'/Data/DMC-O3_RH_15m_dmc-1995-2012_clear'
 df.to_csv(fn)
 
 
 #Creating histograms of data
 #FHIST(spec,dbname,df,Nbins, ext=None)
-#FHIST('O3','DMC',df,50) #PENDING Anchored text
+#FHIST('O3', 'DMC', df, 50) #PENDING Anchored text
 
 
 
