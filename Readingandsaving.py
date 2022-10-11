9ca5313f3917bda7aeeb38bc972a8895f5fb323a
cr2uchile <69597135+cr2uchile@users.noreply.github.com>
2022-10-11 15:35:30 -0300

    Update Readingandsaving.py
    agrege una referencia en la linea 26

--- Readingandsaving.py
+++ Readingandsaving.py
@@ -23,7 +23,7 @@ import pandas as pd              # Data structures
 import matplotlib.pyplot as plt  # Plotting 
 import os as os                  # Directory management    
 import numpy as np               # Numerics   
-import datetime
+import datetime                  # Libraries to time
 from pathlib import Path
 
 #Reading data downloaded from  http://ebas.nilu.no/
